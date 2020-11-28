943194447abfa7d4251b809bb19862ab437d29e9
Alan Melendez <alancuevasmelendez123@gmail.com>
2020-11-28 10:25:08 -0600

    Nuevo

--- htdocs/js/arch.js
+++ htdocs/js/arch.js
@@ -1,8 +1,8 @@
-function Redireccion()
-{
+function Redireccion() {
     alert("Seras redireccionado ala pagina.");
+    window.location.replace("http://facebook.com");
 }
-function EIsesion()
-{
-    
+
+function EIsesion() {
+
 }
